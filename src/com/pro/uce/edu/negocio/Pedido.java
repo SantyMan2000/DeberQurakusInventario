c2ec0cceb70f932cd5ae54754d666c0edfb8846c
Marco Santiago Saltos Pasquel <mssaltosp@uce.edu.ec>
2026-04-17 10:35:18 -0500

    Subiendo logica de notificaciones por montos desde casa

--- src/com/pro/uce/edu/negocio/Pedido.java
+++ src/com/pro/uce/edu/negocio/Pedido.java
@@ -4,10 +4,20 @@ public class Pedido {
 
     private String producto;
     private int cantidad;
+    private double total;
     
-    public Pedido(String producto, int cantidad) {
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Pedido(String producto, int cantidad, double total) {
         this.producto = producto;
         this.cantidad = cantidad;
+        this.total = total;
     }
 
     public String getProducto() {
